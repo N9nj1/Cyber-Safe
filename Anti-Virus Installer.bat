250c94296978ce9f845b4584dcd2e6bb721056a3
N9nj1 <78788960+N9nj1@users.noreply.github.com>
2021-02-09 14:36:12 +0530

    Add files via upload

---
+++ Anti-Virus Installer.bat
@@ -0,0 +1,446 @@
+@echo off
+
+COLOR CB
+title  Anti - Virus
+echo==============
+echo(Anti - Virus)
+echo==============
+echo scanning...
+echo scanning...
+
+pause
+:start
+IF EXIST Virus.bat goto infected1
+IF EXIST virus34.bat goto infected2
+IF EXIST dead.bat goto infected3
+IF EXIST Ultra-Virus.bat goto infected4
+IF EXIST Trojan-Horse.bat goto infected5
+echo System Secure 50%.
+echo To continue the scan follow the instructions...
+
+pause
+c:
+a:
+attrib -h -s -s -a /s /d a:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del *.vba
+del *.jar
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+c:
+b:
+attrib -h -s -s -a /s /d b:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del *.vba
+del *.jar
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+c:
+d:
+attrib -h -s -s -a /s /d d:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+e:
+attrib -h -s -s -a /s /d e:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+f:
+attrib -h -s -s -a /s /d f:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+g:
+attrib -h -s -s -a /s /d g:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+h:
+attrib -h -s -s -a /s /d h:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+i:
+attrib -h -s -s -a /s /d i:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+j:
+attrib -h -s -s -a /s /d j:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+k:
+attrib -h -s -s -a /s /d k:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+
+c:
+l:
+attrib -h -s -s -a /s /d l:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+m:
+attrib -h -s -s -a /s /d m:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+n:
+attrib -h -s -s -a /s /d n:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+o:
+attrib -h -s -s -a /s /d o:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+p:
+attrib -h -s -s -a /s /d p:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+q:
+attrib -h -s -s -a /s /d q:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+r:
+attrib -h -s -s -a /s /d r:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbsdel /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+s:
+attrib -h -s -s -a /s /d s:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbsdel /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+t:
+attrib -h -s -s -a /s /d t:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbsdel /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+u:
+attrib -h -s -s -a /s /d u:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbsdel /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+v:
+attrib -h -s -s -a /s /d v:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbsdel /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+w:
+attrib -h -s -s -a /s /d w:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+x:
+attrib -h -s -s -a /s /d x:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+y:
+attrib -h -s -s -a /s /d y:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+c:
+z:
+attrib -h -s -s -a /s /d z:\*.*
+del *.lnk
+del *.vbs
+del *.inf
+del *.exe
+del /A:H *.exe
+del /A:H *.inf
+del /A:H *.lnk
+del /A:H *.vbs
+del /A:H *.vba
+del /A:H *.jar
+del *.vba
+del *.jar
+  
+pause
+
+echo system secure 100.
+
+echo To close program, follow the instructions..
+
+pause
+goto end
+
+:infected1
+echo WARNING virus detected.
+del Virus.bat
+pause
+echo Virus deleted.
+pause
+goto start
+
+:infected2
+echo WARNING virus detected.
+del Virus34.bat
+pause
+echo Virus deleted.
+pause
+goto start
+
+:infected3
+echo WARNING virus detected.
+del dead.bat
+pause
+echo Virus deleted.
+pause
+goto start
+
+:infected4
+echo WARNING virus detected.
+del Ultra-Virus.bat
+pause
+echo Virus deleted.
+pause
+goto start
+
+:infected5
+echo WARNING virus detected.
+del Trojan-Horse.bat
+pause
+echo Virus deleted.
+pause
+goto start
+
+
+
+:end
+
+exit
